60d35cc3e33970c1e1c7bc3721a0e2920e0975e4
zampovaj <zampovaj.06@spst.eu>
2025-06-02 12:39:53 +0200

    Initial commit + nuget packages

---
+++ CodeAssistant.Libraries/Analyzer.Core/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using CodeAssistant.Application.Interfaces;
+using CodeAssistant.Domain.Interfaces;
+using CodeAssistant.Infrastructure.ApplicationServices;
+using CodeAssistant.Infrastructure.Helpers;
+using CodeAssistant.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeAssistant.Infrastructure
+{
+    /// <summary>
+    /// Provides extension method to register infrastructure-level dependencies.
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Adds services from the Infrastructure layer to the dependency injection container.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<ISyntaxParser, SyntaxParser>();
+            services.AddScoped<ICompilationBuilder, CompilationBuilder>();
+            services.AddScoped<IDiagnosticsMapper, DiagnosticsMapper>();
+            services.AddScoped<ICodeAnalyzer, RoslynCodeAnalyzer>();
+            services.AddScoped<ISolutionAnalyzer, RoslynSolutionAnalyzer>();
+            services.AddScoped<IZipHandler, ZipHandler>();
+            services.AddScoped<IZipExtractor, ZipExtractor>();
+            services.AddScoped<ISolutionFinder, SolutionFinder>();
+            services.AddScoped<ISolutionBuilderService, SolutionMSBuilderService>();
+            services.AddScoped<IPathTrimmer, PathTrimmer>();
+            return services;
+        }
+    }
+
+}
